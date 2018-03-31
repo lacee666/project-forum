34256103d0fbc05cfdcb6ac2c9ca08ca311d4208
Laszlo Bulatovic <laci96zenta@gmail.com>
2018-03-31 22:29:58 +0200

    created frontend pages

--- frontend/forum/src/app/app.module.ts
+++ frontend/forum/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { ForumpageComponent } from './forumpage/forumpage.component';
+import { ForumpostComponent } from './forumpost/forumpost.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    ForumpageComponent,
+    ForumpostComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/forum/src/app/forumpage/forumpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forumpage',
+  templateUrl: './forumpage.component.html',
+  styleUrls: ['./forumpage.component.css']
+})
+export class ForumpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
